e6c933d1eafdfe298d258f33c0eff9b0439c5562
AltherLago <ajsl@ic.ufal.br>
2023-01-16 23:29:35 -0300

    update Route

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
     return (
       <Router>
           <Routes>
-              <Route path="dio-clone-react/" element={<Home />} exact />
-              <Route path="dio-clone-react/login" element={<Login />} exact />
+              <Route path="dio-clone-react/" element={<Home />} />
+              <Route path="dio-clone-react/login" element={<Login />} />
           </Routes>
       </Router>
     );
