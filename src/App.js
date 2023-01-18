ec30dd9a181a42616d26512d0ed9d77e3e9470bb
AltherLago <ajsl@ic.ufal.br>
2023-01-18 20:04:38 -0300

    GlobalStyle

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
-    // Link,
-    // Quando criou um link por algum motivo voltou a funcionar...
-    // useRouteMatch,
-    // useParams
 } from "react-router-dom";
 
-import {Home} from "./pages/home";
+import {GlobalStyle} from "./styles/global";
 
+import {Home} from "./pages/home";
 import {Login} from "./pages/login";
 
 
 function App() {
     return (
       <Router>
+          <GlobalStyle/>
           <Routes>
               <Route path="dio-clone-react/" element={<Home />} />
               <Route path="dio-clone-react/login" element={<Login />} />
